ac00d4a95ddc5804c67262422958bcfc2230c82e
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-04 05:09:01 +0000

    gadgetsモジュールに、files,js,proxy,makeRequest,metadataアクションを追加

--- apps/pc_frontend/modules/gadgets/actions/actions.class.php
+++ apps/pc_frontend/modules/gadgets/actions/actions.class.php
@@ -9,15 +9,94 @@
  */
 class gadgetsActions extends sfActions
 {
+  /**
+   * ex
+   *
+   * @param sfRequest $request A request object
+   * @param HttpServlet $servlet A servlet object
+   */
+  protected function ex($request, $servlet)
+  {
+    $method = "";
+    switch($request->getMethod())
+    {
+      case sfRequest::GET  : $method = 'doGet';  break;
+      case sfRequest::POST : $method = 'doPost'; break;
+    }
+    if (is_callable(array($servlet, $method)))
+    {
+      $servlet->$method();
+    }
+    else
+    {
+      header("HTTP/1.0 405 Method Not Allowed");
+      echo "<html><body><h1>405 Method Not Allowed</h1></body></html>";
+    }
+  }
+  /**
+   * Execute files action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeFiles($request)
+  {
+    $class = new FilesServlet();
+    self::ex($request, $class);
+  }
+
+  /**
+   * Execute js action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeJs($request)
+  {
+    $class = new JsServlet();
+    self::ex($request, $class);
+  }
+
+  /**
+   * Execute proxy action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeProxy($request)
+  {
+    $class = new ProxyServlet();
+    self::ex($request, $class);
+  }
+
+  /**
+   * Execute makeRequest action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeMakeRequest($request)
+  {
+    $_GET['output'] = 'js';
+    $class = new ProxyServlet();
+    self::ex($request, $class);
+  }
+
  /**
-  * Executes index action
+  * Executes ifr action
   *
   * @param sfRequest $request A request object
   */
   public function executeIfr($request)
   {
     $class = new GadgetRenderingServlet();
-    $class->doGet();
-    return sfView::SUCCESS;
+    self::ex($request, $class);
+  }
+
+  /**
+   * Execute metadata action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeMetadata($request)
+  {
+    $class = new MetadataServlet();
+    self::ex($request, $class);
   }
 }
