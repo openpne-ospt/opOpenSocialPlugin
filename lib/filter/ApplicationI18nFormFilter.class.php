1aa7512f2d094a6064e3291078bb3835632f85c3
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-17 08:51:30 +0000

    #3119:symfony1.2への切り替え

---
+++ lib/filter/ApplicationI18nFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ApplicationI18n filter form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage filter
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: sfPropelFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
+ */
+class ApplicationI18nFormFilter extends BaseApplicationI18nFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/filter/ApplicationSettingFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ApplicationSetting filter form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage filter
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: sfPropelFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
+ */
+class ApplicationSettingFormFilter extends BaseApplicationSettingFormFilter
+{
+  public function configure()
+  {
+  }
+}
