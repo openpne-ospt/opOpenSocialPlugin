06b67ef97daa4198d1704de439381720e4904617
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-06 07:44:05 +0000

    opOpenSocialPlugin:added PEAR/Crypt library for OpenPNE3.0.0

--- config/config.php
+++ config/config.php
@@ -2,5 +2,6 @@
 sfToolkit::addIncludePath(array(
   //Shindig
   dirname(__FILE__).'/../lib/vendor/Shindig/',
+  dirname(__FILE__).'/../lib/vendor/PEAR/',
 ));
 $this->dispatcher->connect('routing.load_configuration', array('opOpenSocialPluginRouting', 'listenToRoutingLoadConfigurationEvent'));
