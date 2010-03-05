6ba8a57f2f909b2fbb51b0a9545cd66f0a83ce10
Shogo Kawahara <ooharabucyou@gmail.com>
2010-03-05 22:53:43 +0900

    fixed template of application/info on the pc_frontend to replace deprecated function (fixes #814;BP form #714)

--- apps/pc_frontend/modules/application/templates/infoSuccess.php
+++ apps/pc_frontend/modules/application/templates/infoSuccess.php
@@ -40,8 +40,8 @@ $authorInfoList = array(
   __('Link') => $application->getAuthorLink() ? link_to(null , $application->getAuthorLink(), array('target' => '_blank')) : '',
   __('Quote')   => $application->getAuthorQuote(),
 );
-include_list_box('ApplicationInfoList', $appInfoList, array('title' => __('About App')));
-include_list_box('AuthorInfoList', $authorInfoList, array('title' => __('About Author')));
+op_include_parts('listBox', 'ApplicationInfoList', array('title' => __('About App'), 'list' => $appInfoList));
+op_include_parts('listBox', 'AuthorInfoList', array('title' => __('About Author'), 'list' => $authorInfoList));
 ?>
 
 <?php if ($member->getId() === $application->getMemberId()): ?>
@@ -59,7 +59,7 @@ $developerInfoList = array(
   __('Delete App') => link_to(__('Delete'), '@application_delete?id='.$application->getId()),
 );
 ?>
-<?php include_list_box('DeveloperInfoList', $developerInfoList, array('title' => __('Information for Developer'))); ?>
+<?php op_include_parts('listBox', 'DeveloperInfoList', array('title' => __('Information for Developer'), 'list' => $developerInfoList)); ?>
 <?php endif; ?>
 <div class="moreInfo">
 <ul class="moreInfo">
