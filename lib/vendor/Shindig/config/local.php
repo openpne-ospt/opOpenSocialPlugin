39091c221a00a74489bcf4f208bf2e6bf8e04f52
Shogo Kawahara <ooharabucyou@gmail.com>
2010-04-28 08:42:10 +0900

    fixed config of the libs of Shindig for 'remote_content_fetcher' (fixes #982)

--- lib/vendor/Shindig/config/local.php
+++ lib/vendor/Shindig/config/local.php
@@ -18,7 +18,7 @@ $shindigConfig = array(
   'token_cipher_key' => Doctrine::getTable('SnsConfig')->get('shindig_token_cipher_key'),
   'token_hmac_key'   => Doctrine::getTable('SnsConfig')->get('shindig_token_hmac_key'),
   'token_max_age'    => Doctrine::getTable('SnsConfig')->get('shindig_token_max_age', 60*60),
-  
+
   'base_path'      => sfConfig::get('sf_plugins_dir').'/opOpenSocialPlugin/lib/vendor/Shindig/',
   'features_path'  => sfConfig::get('sf_plugins_dir').'/opOpenSocialPlugin/lib/vendor/Shindig/features/',
   'container_path' => sfConfig::get('sf_app_cache_dir').'/plugins/opOpenSocialPlugin',
@@ -27,10 +27,11 @@ $shindigConfig = array(
   'public_key_file'   => sfConfig::get('sf_plugins_dir').'/opOpenSocialPlugin/certs/public.crt',
   'private_key_phrase' => Doctrine::getTable('SnsConfig')->get('shindig_private_key_phrase', ""), 
 
-  'remote_content'        => 'opShindigRemoteContent',
-  'security_token_signer' => 'opShindigSecurityTokenDecoder',
-  'security_token'        => 'opShindigSecurityToken',
-  'oauth_lookup_service'  => 'opShindigOAuthLookupService',
+  'remote_content'         => 'opShindigRemoteContent',
+  'remote_content_fetcher' => 'opShindigRemoteContentFetcher',
+  'security_token_signer'  => 'opShindigSecurityTokenDecoder',
+  'security_token'         => 'opShindigSecurityToken',
+  'oauth_lookup_service'   => 'opShindigOAuthLookupService',
 
   'data_cache'    => 'opCacheStorageFile',
   'feature_cache' => 'opCacheStorageFile',
