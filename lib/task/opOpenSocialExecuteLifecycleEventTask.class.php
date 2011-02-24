1a4e30dcd8599a2abe2c3203a6948c8d15d75786
Kimura Youichi <kim.upsilon@bucyou.net>
2011-02-24 16:08:37 +0900

    skip sending lifecycle events when a server returned 4xx error codes (refs #1954)

--- lib/task/opOpenSocialExecuteLifecycleEventTask.class.php
+++ lib/task/opOpenSocialExecuteLifecycleEventTask.class.php
@@ -120,6 +120,11 @@ EOF;
           {
             $queue->delete();
           }
+          elseif (floor($response->getStatus() / 100) == 4)
+          {
+            // Client error 4xx (e.g. Not Found)
+            break;
+          }
         }
         catch (Zend_Http_Client_Exception $e)
         {
