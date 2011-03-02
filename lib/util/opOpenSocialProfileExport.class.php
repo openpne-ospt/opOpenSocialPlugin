00cce95e7aa494d5cc8dbf9b052b1b47290a7fdf
Shogo Kawahara <kawahara@bucyou.net>
2011-03-02 14:50:42 +0900

    fixed security issues

--- lib/util/opOpenSocialProfileExport.class.php
+++ lib/util/opOpenSocialProfileExport.class.php
@@ -239,18 +239,28 @@ class opOpenSocialProfileExport extends opProfileExport
     {
       return '';
     }
+    $age = $this->getAge();
+    if (!$age)
+    {
+      // age of the person is private
+      return date('0000/m/d', strtotime($birth));
+    }
 
     return date('Y/m/d', strtotime($birth));
   }
 
   public function getAge()
   {
-    $birth = $this->getBirthday();
-    if (!$birth)
+    if (null !== $this->viewer && $this->viewer->getId() !== $this->member->getId())
     {
-      return '';
+      $age = $this->member->getAge(true, $this->viewer->getId());
     }
-    return (int)(((int)date('Ymd') - (int)date('Ymd', strtotime($birth))) / 10000);
+    else
+    {
+      $age = $this->member->getAge(false, $this->member->getId());
+    }
+
+    return (false !== $age) ? $age : '';
   }
 
   public function getPhoneNumbers()
